cd7fd6c25797168f0ffbc351d8987aa7bfe39a4d
Monika Stokowska <30418684+Monika-Stokowska@users.noreply.github.com>
2017-10-27 12:58:54 +0200

    z poprawkami po rozmowie

--- 9-6.js
+++ 9-6.js
@@ -1,8 +1,10 @@
-document.body.button.addEventListener('click', addElementItem, false);
+var add = document.getElementById('addElem');
+add.addEventListener('click', addElementItem, false);
 function addElementItem() {
     var list = document.getElementById('list');
-    var add = document.getElementById('button');
+    var liElements = document.getElementsByTagName('li');
+    var liLength = liElements.length;
     var newElement = document.createElement('li');
-    newElement.innerHTML='item';
+    newElement.innerHTML='item ' + liLength;
     list.appendChild(newElement);
 } 
